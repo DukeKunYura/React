ab88b4890ffec191e7795e5eedd045e4aeec860f
DukeKunYura <duke_d_yura@rambler.ru>
2022-08-05 01:07:50 +0300

    create components

---
+++ my_app_01/src/components/ShopItemFunc.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './main.css';
+import PropTypes from "prop-types";
+
+
+
+export default function ShopItemFunc(props) {
+    const { brand, title, description, descriptionFull, price, currency } = props
+
+    return (
+        <div className="main-content">
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
+            <h3>{description}</h3>
+            <div className="description">
+                {descriptionFull}
+            </div>
+            <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{currency + price.toFixed(2)}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    )
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.object,
+    brand: PropTypes.string,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    descriptionFull: PropTypes.string,
+    price: PropTypes.number,
+    currency: PropTypes.string
+
+}
