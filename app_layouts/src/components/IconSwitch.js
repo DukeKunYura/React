260e4fbcb3d83450685575b20a03b8518b8727a9
DukeKunYura <110636722+DukeKunYura@users.noreply.github.com>
2022-08-10 23:14:45 +0300

    Delete app_layouts directory

--- app_layouts/src/components/IconSwitch.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MaterialIcons from '@material-ui/icons';
-
-export default function IconSwitch() {
-    return (
-        <IconSwitch icon={"view_list"} onSwitch={() => console.log("change state here")} />
-    )
-}
