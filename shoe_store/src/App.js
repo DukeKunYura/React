728b5ffa575c39edfefae7ac9f609a949b26b01f
DukeKunYura <duke_d_yura@rambler.ru>
2022-10-02 02:01:31 +0300

    create shop

---
+++ shoe_store/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import './css/style.css'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
