1f792e423e1e5752ee78f6548365904d198e0a01
DukeKunYura <duke_d_yura@rambler.ru>
2022-10-03 00:43:49 +0300

    add route in shop

--- shoe_store/src/App.js
+++ shoe_store/src/App.js
@@ -1,11 +1,22 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import CatalogPage from './components/CatalogPage/CatalogPage';
+import AboutPage from './components/AboutPage/AboutPage';
+import ContactsPage from './components/ContactsPage/ContactsPage'
 import './css/style.css'
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route exact path="/home" element={<MainPage />} />
+        <Route path="/catalog.html" element={<CatalogPage />} />
+        <Route path="/about.html" element={<AboutPage />} />
+        <Route path="/contacts.html" element={<ContactsPage />} />
+      </Routes>
       <Footer />
     </>
   );
